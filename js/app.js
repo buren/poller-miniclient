21c4416d1f0a873778011d8056577f41ec070fb1
Jacob Burenstam <burenstam@gmail.com>
2015-10-11 20:50:12 +0200

    Step 2

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ var url = 'https://throwawaypoll.herokuapp.com/result';
 $.getJSON(url, {question: 'test2'}, function(json, textStatus) {
   console.log('Result', json);
 });
+
+var url = 'https://throwawaypoll.herokuapp.com/poll';
+$.getJSON(url, {question: 'test2', value: 'answer1'}, function(json, textStatus) {
+  console.log('Repsonse', json);
+});
