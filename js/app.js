91c1dfff8b298288879b9c7a451a6fcd26b0fde2
Jacob Burenstam <burenstam@gmail.com>
2015-10-11 20:49:17 +0200

    Step 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 console.log('Hello world!');
+
+var url = 'https://throwawaypoll.herokuapp.com/result';
+$.getJSON(url, {question: 'test2'}, function(json, textStatus) {
+  console.log('Result', json);
+});
