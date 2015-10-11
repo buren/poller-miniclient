4d79340331b00fa3e21b7559df7e9be844e1c000
Jacob Burenstam <burenstam@gmail.com>
2015-10-11 20:53:16 +0200

    Step 3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 console.log('Hello world!');
 
-var url = 'https://throwawaypoll.herokuapp.com/result';
-$.getJSON(url, {question: 'test2'}, function(json, textStatus) {
-  console.log('Result', json);
-});
+function getResult() {
+  var url = 'https://throwawaypoll.herokuapp.com/result';
+  $.getJSON(url, {question: 'test2'}, function(json, textStatus) {
+    console.log('Result', json);
+  });
+}
 
-var url = 'https://throwawaypoll.herokuapp.com/poll';
-$.getJSON(url, {question: 'test2', value: 'answer1'}, function(json, textStatus) {
-  console.log('Repsonse', json);
-});
+function submitAnswer(element) {
+  var $el = $(element);
+  var url = 'https://throwawaypoll.herokuapp.com/poll';
+  var value = $el.attr('data-value');
+  $.getJSON(url, {question: 'test2', value: value}, function(json, textStatus) {
+    console.log('Repsonse', json);
+  });
+}
